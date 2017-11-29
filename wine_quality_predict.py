3a6ead7d9eafe88ebc1e5a2a4195bd2333eccac8
MrFiona <1160177283@qq.com>
2017-11-29 23:33:44 +0800

    新增线性回归以及多项式线性回归预测功率以及电流关系文件

--- wine_quality_predict.py
+++ wine_quality_predict.py
@@ -12,9 +12,12 @@ import pandas as pd
 path1 = './datas/winequality-red.csv'
 path2 = './datas/winequality-white.csv'
 
-df1 = pd.read_csv(path1, sep=';')
-df2 = pd.read_csv(path2, sep=';')
+# df1 = pd.read_csv(path1, sep=';')
+# df2 = pd.read_csv(path2, sep=';')
 
-df = df1.append(df2)
+# df = df1.append(df2)
 
+import numpy as np
+
+print np.linspace(1000, 255, 10)
 
